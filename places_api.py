def2485ad892962a347d02c96c03804a9f491ddf
Gabrielle Stein <gabbystein125@gmail.com>
2023-04-13 11:25:31 -0400

    hopefully fix

--- places_api.py
+++ places_api.py
@@ -75,8 +75,8 @@ def write_to_file():
     file.close()
     file2.close()
     
-def get_locations():
+def get_locations(type):
     near_search_all_locs()
     now = datetime.now()
-    loc_dict['Update Time'] = now
+    loc_dict['Update Time'] = {'update time': now, 'type': type}
     return json.dumps(loc_dict)
